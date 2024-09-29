ba09ea9b5c43721d950d98f53290f11d9bca0d7b
CBoswell963 <142553717+CBoswell963@users.noreply.github.com>
2024-09-29 14:46:04 -0400

    Add files via upload

---
+++ Senior Design Project/cops_platform/policies/course_manager/compile_alias.sh
@@ -0,0 +1,3 @@
+#You can load this alias by just running 'source compile_alias.sh'
+alias secompile="make -f /usr/share/selinux/devel/Makefile course_manager.pp"
+alias sebuild="sudo semodule -i course_manager.pp"
